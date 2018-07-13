d951d22285f0d1a93606387c11d404a0e9de7d96
lavenderx <dolphineor@gmail.com>
2018-07-13 13:58:59 +0800

    Update config

--- consul/src/main/java/com/github/lavenderx/demo/ConsulApplication.java
+++ consul/src/main/java/com/github/lavenderx/demo/ConsulApplication.java
@@ -3,10 +3,22 @@ package com.github.lavenderx.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * https://springcloud.cc/spring-cloud-consul.html
+ * https://blog.csdn.net/zjcjava/article/details/78608892
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ConsulApplication {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsulApplication.class, args);
